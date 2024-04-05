04572a401445b985134eae92062b4af0734d027c
jkane15 <118020278+jkane15@users.noreply.github.com>
2024-04-04 23:53:23 -0400

    Update M03_Lab_Lists_Functions_and_Classes.py

--- M03_Lab_Lists_Functions_and_Classes.py
+++ M03_Lab_Lists_Functions_and_Classes.py
@@ -1,3 +1,17 @@
+#Author: John Kane 
+#File name: M03_lb_lists_functions_and_classes
+#Description:
+    #App that will accept user input for a car. The app will store "car" into the vehicle type in your Vehicle super class. 
+    #The app will then ask the user for the year, make, model, doors, and type of roof and store thdata in the attributes above.
+    #The app will then output the data in an easy-to-read and understandable format
+# Variables:
+# - vehicle_type: A string representing the type of vehicle (e.g., "car").
+# - year: An integer representing the year of the car.
+# - make: A string representing the make of the car (e.g., "Toyota").
+# - model: A string representing the model of the car (e.g., "Camry").
+# - doors: An integer representing the number of doors the car has (either 2 or 4).
+# - roof: A string representing the type of roof on the car (e.g., "solid" or "sun roof").
+
 class Vehicle:
     def __init__(self, vehicle_type):
         self.vehicle_type = vehicle_type
